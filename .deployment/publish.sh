ed0d9241d86e487ee740dd7397cdae65b0343168
Pouyan Heyratpour <me@pouyan.dev>
2025-05-17 01:32:41 +0330

    refactor: better rendering on all media
    Signed-off-by: Pouyan Heyratpour <me@pouyan.dev>

--- .deployment/publish.sh
+++ .deployment/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source .env
-lftp -e "set ftp:ssl-allow no; mirror -R --exclude-glob=.* --exclude-glob=.*/ --exclude-glob=*.md -n -e --delete-excluded -P=10 -v; quit" -u "${USERNAME}","${PASSWORD}" "${HOSTNAME}"
+lftp -e "set ssl:verify-certificate no; set ftp:prefer-epsv yes; mirror -R --exclude-glob=.* --exclude-glob=.*/ --exclude-glob=*.md -n -e --delete-excluded -P=10 -v; quit" -u "${FTP_USERNAME}","${FTP_PASSWORD}" "${FTP_HOSTNAME}"
